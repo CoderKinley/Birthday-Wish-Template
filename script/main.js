c6171629bb2103d5b08e2123f352e2e7f9f2f3dd
CoderKinley <kinleypenjor13@gmail.com>
2024-02-10 18:36:12 +0530

    new changes:

--- script/main.js
+++ script/main.js
@@ -71,7 +71,7 @@ const animationTimeline = () => {
     .join("</span><span>")}</span`;
 
   hbd.innerHTML = `<span>${hbd.innerHTML
-    .split("")
+    .split(" ")
     .join("</span><span>")}</span`;
 
   const ideaTextTrans = {
